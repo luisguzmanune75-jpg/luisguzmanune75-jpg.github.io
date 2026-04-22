b10c92f2e4dbb61686e363d0f6252a5e299fb3c7
luisguzmanune75-jpg <luisguzmanune75@gmail.com>
2026-04-22 15:47:33 +0200

    Fix CORS headers in Netlify news function

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -8,6 +8,11 @@ export async function handler(event) {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET"
+      },
       body: JSON.stringify(data.articles)
     };
 
