19a251101fd6006043b294cf509deb5923609741
luisguzmanune75-jpg <luisguzmanune75@gmail.com>
2026-06-24 19:56:28 +0200

    Restore direct YouTube SNG TV flow

--- sng-tv-config.js
+++ sng-tv-config.js
@@ -5,6 +5,6 @@ const API_KEY = "AIzaSyDqun2dmiABDnIwNyiBr5QIhs5PNA4KXBE";
 
 // Tu peux mettre ici un vrai Channel ID YouTube de type "UC...".
 // La valeur actuelle est une URL de handle ; le script la résout automatiquement.
-const CHANNEL_ID = "https://www.youtube.com/@CanalFamilial";
+const CHANNEL_ID = "https://www.youtube.com/@CanalFamilial/shorts";
 
-const MAX_RESULTS = 12;
+const MAX_RESULTS = 20;
